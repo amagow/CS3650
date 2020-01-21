9478b39a0a72f5fc59c1b55befbfe761b8fc4458
Adwithya <avmagow@gmail.com>
2020-01-20 23:37:54 -0500

    Error code fixed

---
+++ hw02-starter/calc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if(argc != 4 || (strncmp(argv[2], "+", 8) && strncmp(argv[2], "-", 8) && strncmp(argv[2], "*", 8) && strncmp(argv[2], "/", 8) ) ){
+        puts("Usage:\n./ccalc N op N");
+        printf("%d\n", strncmp(argv[2], "+", 8));
+        return 1;
+    }
+
+    int x = atoi(argv[1]);
+    int y = atoi(argv[3]);
+    String fmt = "%d %s %d = %d\n";
+
+    if(!strncmp(argv[2], "+", 8))
+        printf(fmt, x, argv[2], y, x + y);
+
+    if(!strncmp(argv[2], "-", 8))
+        printf(fmt, x, argv[2], y, x - y);
+    
+    if(!strncmp(argv[2], "*", 8))
+        printf(fmt, x, argv[2], y, x * y);
+    
+    if(!strncmp(argv[2], "/", 8))
+        printf(fmt, x, argv[2], y, x / y);
+
+    return 0;
+}

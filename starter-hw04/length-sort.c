145e17741bef491c763fbbfbedc8b11ed5c75ece
Adwithya <avmagow@gmail.com>
2020-02-04 14:49:11 -0500

    Chomp

--- starter-hw04/length-sort.c
+++ starter-hw04/length-sort.c
@@ -14,44 +14,50 @@ void
 chomp(char* text)
 {
     // TODO: Modify input string to remove trailing newline ('\n')
+    strtok(text, "\n");
 }
 
-int
-main(int argc, char* argv[])
-{
-    if (argc != 2) {
-        printf("Usage:\n  %s input-file\n", argv[0]);
-        return 1;
-    }
+// int
+// main(int argc, char* argv[])
+// {
+//     if (argc != 2) {
+//         printf("Usage:\n  %s input-file\n", argv[0]);
+//         return 1;
+//     }
 
-    FILE* fh = fopen(argv[1], "r");
-    if (!fh) {
-        perror("open failed");
-        return 1;
-    }
+//     FILE* fh = fopen(argv[1], "r");
+//     if (!fh) {
+//         perror("open failed");
+//         return 1;
+//     }
 
-    svec* xs = make_svec();
-    char temp[128];
+//     svec* xs = make_svec();
+//     char temp[128];
 
-    while (1) {
-        char* line = fgets(temp, 128, fh);
-        if (!line) {
-            break;
-        }
+//     while (1) {
+//         char* line = fgets(temp, 128, fh);
+//         if (!line) {
+//             break;
+//         }
 
-        chomp(line);
-        svec_push_back(xs, line);
-    }
+//         chomp(line);
+//         svec_push_back(xs, line);
+//     }
 
-    fclose(fh);
+//     fclose(fh);
 
-    sort(xs);
+//     sort(xs);
 
-    for (int ii = 0; ii < xs->size; ++ii) {
-        char* line = svec_get(xs, ii);
-        printf("%s\n", line);
-    }
+//     for (int ii = 0; ii < xs->size; ++ii) {
+//         char* line = svec_get(xs, ii);
+//         printf("%s\n", line);
+//     }
 
-    free_svec(xs);
-    return 0;
+//     free_svec(xs);
+//     return 0;
+// }
+int main(int argc, char* argv[]){
+    char* something = "Hi magow\n";
+    chomp(something);
+    printf("%s", something);    
 }

1c38b8bbd65900540ca9e21cb3843e443631c9c6
Adwithya <avmagow@gmail.com>
2020-02-05 00:24:14 -0500

    .

--- starter-hw04/test_hash.c
+++ starter-hw04/test_hash.c
@@ -8,11 +8,11 @@
 
 int main(int argc, char* argv[]){
     hashmap* hash = make_hashmap();
-    map_put(hash, "abc", 1);
-    map_put(hash, "def", 1);
-    map_put(hash, "dca", 1);
-    map_put(hash, "qow", 1);
+    hashmap_put(hash, "abc", 1);
+    hashmap_put(hash, "def", 1);
+    hashmap_put(hash, "dca", 1);
+    hashmap_put(hash, "qow", 1);
 
-    printf("%s\t%d\n", "abc" ,map_get(hash, "abc"));
+    printf("%s\t%d\n", "abc" ,hashmap_get(hash, "abc"));
     hashmap_dump(hash);
 } 

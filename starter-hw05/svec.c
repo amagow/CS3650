69cec77a81e557baa57c682ab70f457d79c91d11
Adwithya <avmagow@gmail.com>
2020-02-11 18:13:40 -0500

    testing code

---
+++ starter-hw05/svec.c
@@ -0,0 +1,95 @@
+/* This file is lecture notes from CS 3650, Fall 2018 */
+/* Author: Nat Tuck */
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "svec.h"
+
+//If you are a TA reading this assignment please answer these questions in the feedback
+
+svec *
+make_svec()
+{
+    svec *sv = malloc(sizeof(svec));
+    sv->data = malloc(2 * sizeof(char *)); //Why use malloc and not use calloc
+    memset(sv->data, 0, 2 * sizeof(char *));
+    sv->size = 0;
+    sv->capacity = 2;
+    return sv;
+}
+
+void free_svec(svec *sv)
+{
+    for (int ii = 0; ii < sv->size; ++ii)
+    {
+        if (sv->data[ii])
+        {
+            free(sv->data[ii]);
+        }
+    }
+    free(sv->data);
+    free(sv);
+}
+
+char *
+svec_get(svec *sv, int ii)
+{
+    assert(ii >= 0 && ii < sv->size);
+    return sv->data[ii];
+}
+
+void svec_put(svec *sv, int ii, char *item)
+{
+    assert(ii >= 0 && ii < sv->size);
+    sv->data[ii] = strdup(item);
+}
+
+void svec_push_back(svec *sv, char *item)
+{
+    int ii = sv->size;
+
+    if (ii >= sv->capacity)
+    {
+        sv->capacity *= 2;
+        sv->data = realloc(sv->data, sv->capacity * sizeof(char *));
+    }
+
+    sv->size = ii + 1;
+    svec_put(sv, ii, item);
+}
+
+void svec_swap(svec *sv, int ii, int jj)
+{
+    char *temp = sv->data[ii];
+    sv->data[ii] = sv->data[jj];
+    sv->data[jj] = temp;
+}
+
+svec *
+reverse(svec *sv)
+{
+    svec *sv2 = make_svec();
+    for (long ii = sv->size; ii >= 0; --ii)
+    {
+        svec_push_back(sv2, sv->data[ii]);
+    }
+    return sv2;
+}
+
+svec *
+reverse_free(svec *sv)
+{
+    svec *sv2 = reverse(sv);
+    free(sv);
+    return sv2;
+}
+
+void print_vec(svec *sv)
+{
+    for(long ii = 0; ii < sv->size; ++ii){
+        puts(sv->data[ii]);
+    }
+}
--- starter-hw05/tokens.c
+++ starter-hw05/tokens.c
@@ -1,3 +1,103 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "svec.h"
+
+void chomp(char *text)
+{
+    // int n = strlen(text);
+    // if(text[n-1] == "\n")
+    //     text[n-1] = 0;
+    strtok(text, "\n");
+}
+
+char *read_line(char *text)
+{
+   char *rv = fgets(text, 1000, stdin);
+   if (!rv)
+   {
+      exit(0);
+   }
+   return text;
+}
+
+char *
+read_argument(const char* text, long ii)
+{
+   long nn = 0;
+   while (!isblank(text[ii + nn]))
+   {
+      ++nn;
+   }
+   
+   char* arg = malloc(nn + 1);
+   memcpy(arg, text + ii, nn);
+   arg[nn] = 0;
+   return arg;
+}
+
+svec *
+tokenize(char *text)
+{
+   svec *sv = make_svec();
+   //Length of the text
+   long len = strlen(text);
+   long ii = 0;
+
+   while (ii < len)
+   {
+      if (isspace(text[ii]))
+      {
+         ++ii;
+         continue;
+      }
+
+      if ((text[ii] == '|' && text[ii + 1] == '|') || (text[ii] == '&' && text[ii + 1] == '&'))
+      {
+         char op[] = "xx";
+         op[0] = text[ii];
+         op[1] = text[ii + 1];
+         svec_push_back(sv, op);
+         ++ii;
+         continue;
+      }
+
+      if (text[ii] == '<' || text[ii] == '>' || text[ii] == ';' || text[ii] == '|' || text[ii] == '&')
+      {
+         char op[] = "x";
+         op[0] = text[ii];
+         svec_push_back(sv, op);
+         ++ii;
+         continue;
+      }
+
+      char *arg = read_argument(text, ii);
+      svec_push_back(sv, arg);
+      ii += strlen(arg);
+      free(arg);
+   }
+   return sv;
+}
+
+int main(int argc, char const *argv[])
+{
+   char text[1024];
+   //Create an array that stores 1024 chars
+   while (1)
+   {
+      printf("tokens$ ");
+      fflush(stdout);
+      chomp(text);
+      char *line = read_line(text);
+      svec *toks = tokenize(line);
+      printf("value of string: %s", line);
+      toks = reverse_free(toks);
+      print_vec(toks);
+   }
+
+   return 0;
+}
 
 /* TODO:
 
@@ -14,7 +114,3 @@
    }
 
 */
-
-
-
-

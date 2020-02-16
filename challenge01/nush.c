bbd4cca15950b5b9511331a59ffb18527424d433
Adwithya <avmagow@gmail.com>
2020-02-16 17:17:57 -0500

    Cleanup

--- challenge01/nush.c
+++ challenge01/nush.c
@@ -11,20 +11,27 @@
 
 void eval(char *cmd, char *const args[], int size)
 {
-
-    char *targ[size + 1];
-    for (size_t i = 0; i < size; i++)
+    int cpid;
+    if ((cpid = fork()))
     {
-        targ[i] = args[i];
+        int status;
+        waitpid(cpid, &status, 0);
     }
-    targ[size] = NULL;
+    else
+    {
+        char *targ[size + 1];
+        for (size_t i = 0; i < size; i++)
+        {
+            targ[i] = args[i];
+        }
+        targ[size] = NULL;
 
-    execvp(cmd, targ);
+        execvp(cmd, targ);
+    }
 }
 
 void execute(char *cmd)
 {
-    int cpid;
     svec *args = tokenize(cmd);
     if (!strcmp(args->data[0], "exit"))
     {
@@ -36,40 +43,7 @@ void execute(char *cmd)
         chdir(targ);
         return;
     }
-    if ((cpid = fork()))
-    {
-        // parent process
-        // printf("Parent pid: %d\n", getpid());
-        // printf("Parent knows child pid: %d\n", cpid);
-
-        // Child may still be running until we wait.
-
-        int status;
-        waitpid(cpid, &status, 0);
-
-        // printf("== executed program complete ==\n");
-
-        // printf("child returned with wait code %d\n", status);
-        if (WIFEXITED(status))
-        {
-            // printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
-        }
-    }
-    else
-    {
-        // child process
-        // printf("Child pid: %d\n", getpid());
-        // printf("Child knows parent pid: %d\n", getppid());
-
-        // The argv array for the child.
-        // Terminated by a null pointer.
-        // char* args[] = {cmd, "one", 0};
-
-        eval(args->data[0], args->data, args->size);
-        // printf("== executed program's output: ==\n");
-        // execvp(cmd, args);
-        // printf("Can't get here, exec only returns on error.");
-    }
+    eval(args->data[0], args->data, args->size);
     free_svec(args);
 }
 

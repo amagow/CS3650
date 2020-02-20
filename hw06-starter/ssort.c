072926486d97a3b64e0ea145a5715b6916585a9f
Adwithya <avmagow@gmail.com>
2020-02-20 16:19:10 -0500

    .

--- hw06-starter/ssort.c
+++ hw06-starter/ssort.c
@@ -109,7 +109,7 @@ int main(int argc, char *argv[])
     long count = fileCount[0];
     
     float *fileArray = mmap(0, count * sizeof(float), PROT_READ,
-                      MAP_FILE | MAP_PRIVATE, fd, 0);                  
+                      MAP_FILE | MAP_PRIVATE, fd, sizeof(long));                  
     float *data = fileArray;
 
     printf("%ld\n", count);

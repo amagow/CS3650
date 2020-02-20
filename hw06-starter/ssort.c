f1ed01720e39e12cfda507fd03a7beede4e60242
Adwithya <avmagow@gmail.com>
2020-02-20 15:36:16 -0500

    hw06

--- hw06-starter/ssort.c
+++ hw06-starter/ssort.c
@@ -12,24 +12,28 @@
 #include "barrier.h"
 #include "utils.h"
 
-void
-qsort_floats(floats* xs)
+int comp(const void * a, const void * b){
+    return ( *(long*)a - *(long*)b );
+}
+
+    void qsort_floats(floats *xs)
 {
     // TODO: call qsort to sort the array
     // see "man 3 qsort" for details
+    //FIX : length of the array
+    qsort(xs, 0, sizeof(long), comp);
 }
 
-floats*
-sample(float* data, long size, int P)
+floats *
+sample(float *data, long size, int P)
 {
     // TODO: sample the input data, per the algorithm decription
     return make_floats(10);
 }
 
-void
-sort_worker(int pnum, float* data, long size, int P, floats* samps, long* sizes, barrier* bb)
+void sort_worker(int pnum, float *data, long size, int P, floats *samps, long *sizes, barrier *bb)
 {
-    floats* xs = make_floats(10);
+    floats *xs = make_floats(10);
     // TODO: select the floats to be sorted by this worker
 
     printf("%d: start %.04f, count %ld\n", pnum, samps->data[pnum], xs->size);
@@ -43,42 +47,44 @@ sort_worker(int pnum, float* data, long size, int P, floats* samps, long* sizes,
     free_floats(xs);
 }
 
-void
-run_sort_workers(float* data, long size, int P, floats* samps, long* sizes, barrier* bb)
+void run_sort_workers(float *data, long size, int P, floats *samps, long *sizes, barrier *bb)
 {
     pid_t kids[P];
-    (void) kids; // suppress unused warning
+    (void)kids; // suppress unused warning
 
     // TODO: spawn P processes, each running sort_worker
 
-    for (int ii = 0; ii < P; ++ii) {
+    for (int ii = 0; ii < P; ++ii)
+    {
         //int rv = waitpid(kids[ii], 0, 0);
         //check_rv(rv);
     }
 }
 
-void
-sample_sort(float* data, long size, int P, long* sizes, barrier* bb)
+void sample_sort(float *data, long size, int P, long *sizes, barrier *bb)
 {
-    floats* samps = sample(data, size, P);
+    floats *samps = sample(data, size, P);
     run_sort_workers(data, size, P, samps, sizes, bb);
     free_floats(samps);
 }
 
-int
-main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     alarm(120);
 
-    if (argc != 3) {
+    //Usage error is args are not equal to three
+    if (argc != 3)
+    {
         printf("Usage:\n");
         printf("\t%s P data.dat\n", argv[0]);
         return 1;
     }
 
+    //Get inputs
     const int P = atoi(argv[1]);
-    const char* fname = argv[2];
+    const char *fname = argv[2];
 
+    //Set seed for the next few random calls
     seed_rng();
 
     int rv;
@@ -87,34 +93,39 @@ main(int argc, char* argv[])
     check_rv(rv);
 
     const int fsize = st.st_size;
-    if (fsize < 8) {
+    if (fsize < 8)
+    {
         printf("File too small.\n");
         return 1;
     }
 
+    //Open file
     int fd = open(fname, O_RDWR);
     check_rv(fd);
 
+    //FIX: unknown size??
     int size = 1024;
 
-    void* file = mmap(0,size,PROT_READ,
-                     MAP_FILE|MAP_PRIVATE, fd, 0); // TODO: load the file with mmap.
-    (void) file; // suppress unused warning.
+    long *file = mmap(0, size, PROT_READ,
+                      MAP_FILE | MAP_PRIVATE, fd, 0); // TODO: load the file with mmap.
 
     // TODO: These should probably be from the input file.
-    long count = P;
-    float* data = file;
+    long count = 0;
+    while(file[count]){
+        count ++;
+    }
+    float *data = file;
 
     printf("%ld", count);
     printf("%f", data[0]);
 
     long sizes_bytes = P * sizeof(long);
-    long* sizes = mmap(0,sizes_bytes,PROT_READ|PROT_WRITE,
-                     MAP_SHARED|MAP_ANONYMOUS, -1, 0); // TODO: This should be shared
+    long *sizes = mmap(0, sizes_bytes, PROT_READ | PROT_WRITE,
+                       MAP_SHARED | MAP_ANONYMOUS, -1, 0); // TODO: This should be shared
 
-    barrier* bb = make_barrier(P);
+    barrier *bb = make_barrier(P);
 
-    sample_sort(data, count, P, sizes, bb);
+    // sample_sort(data, count, P, sizes, bb);
 
     free_barrier(bb);
 
@@ -122,4 +133,3 @@ main(int argc, char* argv[])
     munmap(file, size);
     return 0;
 }
-

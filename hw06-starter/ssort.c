5b14c177197fd55e6cd83ee7dfe9c3045d26915c
Adwithya <avmagow@gmail.com>
2020-02-23 16:29:18 -0500

    .

--- hw06-starter/ssort.c
+++ hw06-starter/ssort.c
@@ -17,11 +17,11 @@ int comp(const void *a, const void *b)
     float fa = *(const float *)a;
     float fb = *(const float *)b;
     if (fa > 99.99 && fa < 100){
-        printf(":fa%f fb:%f", fa, fb);
+        printf(":fa%f fb:%f\n", fa, fb);
         return -1;
     }
     if (fb > 99.99 && fb < 100){
-        printf(":fa%f fb:%f", fa, fb);
+        printf(":fa%f fb:%f\n", fa, fb);
         return -1;
     }
     return (fa > fb) - (fa < fb);

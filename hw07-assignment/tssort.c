86f23abc62be88be8fd4563814cb284d2cecf2d9
Adwithya <avmagow@gmail.com>
2020-03-11 12:26:00 -0400

    .

--- hw07-assignment/tssort.c
+++ hw07-assignment/tssort.c
@@ -180,7 +180,7 @@ int main(int argc, char *argv[])
     }
 
     //Open file
-    int fd = open(fname, 0644);
+    int fd = open(fname, O_RDWR);
     int fd2 = open(fname2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     check_rv(fd);
     check_rv(fd2);

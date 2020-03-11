d42a2c871408d7e6937e2c46fcf06712720ac756
Adwithya <avmagow@gmail.com>
2020-03-11 03:52:53 -0400

    .

--- hw07-assignment/tssort.c
+++ hw07-assignment/tssort.c
@@ -1,231 +1,3 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <sys/mman.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <sys/wait.h>
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <math.h>
-// #include <float.h>
-// #include <assert.h>
-// #include "float_vec.h"
-// #include "barrier.h"
-// #include "utils.h"
-
-// typedef struct sort_job
-// {
-//     int pnum;
-//     float *readData;
-//     float *writeData;
-//     long size;
-//     int P;
-//     floats *samps;
-//     long *sizes;
-//     barrier *bb;
-// } sort_job;
-
-// int comp(const void *a, const void *b)
-// {
-//     float fa = *(const float *)a;
-//     float fb = *(const float *)b;
-//     return (fa > fb) - (fa < fb);
-// }
-
-// void qsort_floats(floats *xs)
-// {
-//     qsort(xs->data, xs->size, sizeof(float), comp);
-// }
-
-// floats *
-// sample(float *data, long size, int P)
-// {
-//     floats *xs = make_floats(0);
-
-//     floats_push(xs, FLT_MIN);
-
-//     int range = 3 * (P - 1);
-//     float tmp[range];
-//     for (int ii = 0; ii < range; ++ii)
-//     {
-//         tmp[ii] = data[rand() % size];
-//     }
-//     qsort(tmp, range, sizeof(float), comp);
-//     for (int ii = 1; ii < range; ii += 3)
-//     {
-//         floats_push(xs, tmp[ii]);
-//     }
-
-//     floats_push(xs, FLT_MAX);
-
-//     return xs;
-// }
-
-// void *sort_worker(void *arg)
-// {
-//     sort_job *t_arg = ((sort_job *)arg);
-
-//     floats *xs = make_floats(0);
-//     for (int ii = 0; ii < t_arg->size; ii++)
-//     {
-//         if (t_arg->readData[ii] > t_arg->samps->data[t_arg->pnum] && t_arg->readData[ii] <= t_arg->samps->data[t_arg->pnum + 1])
-//         {
-//             floats_push(xs, t_arg->readData[ii]);
-//         }
-//     }
-
-//     for (int ii = 0; ii < xs->size; ii++)
-//     {
-//         assert(xs->data[ii] > t_arg->samps->data[t_arg->pnum] && xs->data[ii] <= t_arg->samps->data[t_arg->pnum + 1]);
-//     }
-
-//     printf("%d: start %.04f, count %ld\n", t_arg->pnum, t_arg->samps->data[t_arg->pnum], xs->size);
-//     t_arg->sizes[t_arg->pnum] = xs->size;
-
-//     printf("reached 0 %d\n", t_arg->pnum);
-//     barrier_wait(t_arg->bb);
-
-//     printf("reached 1 %d\n", t_arg->pnum);
-//     long start = 0, end = 0;
-//     for (int ii = 0; ii < t_arg->pnum; ii++)
-//     {
-//         start += t_arg->sizes[ii];
-//     }
-//     for (int ii = 0; ii <= t_arg->pnum; ii++)
-//     {
-//         end += t_arg->sizes[ii];
-//     }
-//     // printf("process %d\n", pnum);
-//     qsort_floats(xs);
-//     // floats_print(xs);
-//     barrier_wait(t_arg->bb);
-//     printf("reached 2 %d\n", t_arg->pnum);
-//     for (int ii = start; ii < end; ii++)
-//     {
-//         // printf("process %d, stores float %F at %d\n", pnum, xs->data[ii], start + ii);
-//         assert(xs->data[ii - start] > t_arg->samps->data[t_arg->pnum] && xs->data[ii - start] <= t_arg->samps->data[t_arg->pnum + 1]);
-//         t_arg->writeData[ii] = xs->data[ii - start];
-//     }
-//     free_floats(xs);
-//     free(t_arg);
-//     return 0;
-// }
-
-// void run_sort_workers(float *readData, float *writeData, long size, int P, floats *samps, long *sizes, barrier *bb)
-// {
-//     pthread_t threads[P];
-
-//     for (int pp = 0; pp < P; ++pp)
-//     {
-//         sort_job *arg = malloc(sizeof(sort_job));
-//         arg->pnum = pp;
-//         arg->readData = readData;
-//         arg->writeData = writeData;
-//         arg->size = size;
-//         arg->P = P;
-//         arg->samps = samps;
-//         arg->sizes = sizes;
-//         arg->bb = bb;
-
-//         int rv = pthread_create(&(threads[pp]), 0, sort_worker, (void *)arg);
-//         check_rv(rv);
-//     }
-
-//     for (int pp = 0; pp < P; ++pp)
-//     {
-//         int rv = pthread_join(threads[pp], 0);
-//         check_rv(rv);
-//     }
-// }
-
-// void sample_sort(float *readData, float *writeData, long size, int P, long *sizes, barrier *bb)
-// {
-//     floats *samps = sample(readData, size, P);
-//     run_sort_workers(readData, writeData, size, P, samps, sizes, bb);
-//     free_floats(samps);
-// }
-
-// int main(int argc, char *argv[])
-// {
-//     alarm(120);
-
-//     //Usage error is args are not equal to three
-//     if (argc != 4)
-//     {
-//         printf("Usage:\n");
-//         printf("\t%s P input.dat output.dat\n", argv[0]);
-//         return 1;
-//     }
-
-//     //Get inputs
-//     const int P = atoi(argv[1]);
-//     const char *fname = argv[2];
-//     const char *fname2 = argv[3];
-
-//     //Set seed for the next few random calls
-//     seed_rng();
-
-//     int rv;
-//     struct stat st;
-//     rv = stat(fname, &st);
-//     check_rv(rv);
-
-//     const int fsize = st.st_size;
-//     if (fsize < 8)
-//     {
-//         printf("File too small.\n");
-//         return 1;
-//     }
-
-//     //Open file
-//     int fd = open(fname, O_RDWR);
-//     int fd2 = open(fname2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-//     check_rv(fd);
-//     check_rv(fd2);
-
-//     //Open mmaps
-//     long count;
-//     read(fd, &count, 8);
-//     float *fileArray = mmap(0, (count + 2) * sizeof(float), PROT_READ,
-//                             MAP_FILE | MAP_SHARED, fd, 0);
-
-//     write(fd2, &count, 8);
-//     float *writeArray = mmap(0, (count + 2) * sizeof(float), PROT_WRITE,
-//                              MAP_FILE | MAP_SHARED, fd2, 0);
-//     //Next few bits are for the array data
-//     float *readData = &fileArray[2];
-//     float *writeData = &writeArray[2];
-
-//     long sizes_bytes = P * sizeof(long);
-//     long *sizes = mmap(0, sizes_bytes, PROT_READ | PROT_WRITE,
-//                        MAP_SHARED | MAP_ANONYMOUS, -1, 0); // TODO: This should be shared
-
-//     // Debugger output
-//     // for (size_t i = 0; i < count; i++)
-//     // {
-//     //     printf("%f\n", data[i]);
-//     // }
-
-//     barrier *bb = make_barrier(P);
-//     sample_sort(readData, writeData, count, P, sizes, bb);
-
-//     free_barrier(bb);
-
-//     // for (size_t i = 0; i < count; i++)
-//     // {
-//     //     printf("%f\n", data[i]);
-//     // }
-
-//     munmap(sizes, sizes_bytes);
-//     munmap(fileArray, (count + 2) * sizeof(float));
-//     munmap(writeArray, (count + 2) * sizeof(float));
-
-//     //Close File descriptor
-//     close(fd2);
-//     close(fd);
-//     return 0;
-// }
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -241,255 +13,156 @@
 #include "barrier.h"
 #include "utils.h"
 
-typedef struct job {
-int pnum;
-float* data;
-long size;
-int P;
-floats* samps;
-long* sizes;
-barrier* bb;
-} job;
-
-
-float*
-readinput(int fd, long count){
-float* data = malloc(4 * count);
-
-for(int ii = 0; ii < count; ++ii){
-	read(fd, &data[ii], 4);
-}
-
-return data;
-
-}
-
-
-
-void
-writeoutput(const char* file, long count, float* data){
-
-int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-check_rv(fd);
-
-write(fd, &count, 8);
-for(int ii = 0; ii < count; ++ii){
-	write(fd, &data[ii], 4);
-}
-
-close(fd);
-}
-
-
-
-long 
-sumsizes_start(long* sizes, int pp){
-//Summing sizes to calculate the start index on the file to copy the sorted data. 
-if(pp == 0) return 0;
-
-long result = 0;
-for (int cc = 0; cc <= pp - 1; ++cc) {
-	result += sizes[cc];
-}
-
-return result;
-}
-
-
-
-long
-sumsizes_end(long* sizes, int pp){
-//Summing sizes to calculate the end index ont he file to copy the sorted data. 
-long result = 0;
-for(int cc = 0; cc <= pp; ++cc){
-	result += sizes[cc];
-}
-
-return result;
-}
-
-int
-comparator(const void* p1, const void* p2){
-
-//Comparator implemented for qsort function. 
-float f1 = *(const float *)p1;
-float f2 = *(const float *)p2;
-
-if(f1 < f2){
-	return -1;
-} else if (f1 == f2){
-	return 0;
-} else {
-	return 1;
-}
-}
-
-
-void
-qsort_floats(floats* xs)
+typedef struct sort_job
 {
-// Call qsort to sort the array.
-qsort(xs->data, xs->size, 4, comparator);
-}
+    int pnum;
+    float *readData;
+    float *writeData;
+    long size;
+    int P;
+    floats *samps;
+    long *sizes;
+    barrier *bb;
+} sort_job;
 
-
-
-
-floats*
-sample(float* data, long size, int P)
+int comp(const void *a, const void *b)
 {
-
-// 1- Randomly select 3*(P-1) items from the array.
-floats* sample_items = make_floats(3 * (P - 1));
-
-for(int ii = 0; ii < sample_items->cap; ++ii){
-float chosen = data[rand() % size];
-floats_push(sample_items, chosen);
+    float fa = *(const float *)a;
+    float fb = *(const float *)b;
+    return (fa > fb) - (fa < fb);
 }
 
-// 2 - Sort chosen items.
-qsort_floats(sample_items);
-//floats_print(sample_items);
-
-
-// 3 - Take the median of each group of 3 in the sorted sample_items producing our array of floats samps. 
-floats* samps = make_floats(P + 1);
-// 4 - Adding 0 at start.
-floats_push(samps, 0);
-int median = 1;
-for(int ii = 0; ii < P-1; ++ii){
-    floats_push(samps, sample_items->data[median]);
-    median += 3;
-}
-
-
-free_floats(sample_items);
-
-// 4 - Adding maximun value at the end.
-floats_push(samps, FLT_MAX);
-
-
-return samps;
-}
-
-
-
-
-void*
-sort_worker(void *args) {
-
-//NOTE: maybe create a wrapper function to make it easier to work with input arguments. 
-
-job* arg = ((job*)args);
-
-floats* xs = make_floats(arg->size / arg->P);
-
-
-//Selecting fooats to be sorted by this worker. 
-float start = arg->samps->data[arg->pnum];
-float end = arg->samps->data[arg->pnum + 1];
-
-for(int ii = 0; ii < arg->size; ++ii){
-if (arg->data[ii] >= start && arg->data[ii] < end){
-	floats_push(xs, arg->data[ii]);
-}
-}
-
-//Writting the number of items taken to a shared array of sizes at slot p.
-arg->sizes[arg->pnum] = xs->size;
-
-printf("%d: start %.04f, count %ld\n", arg->pnum, arg->samps->data[arg->pnum], arg->sizes[arg->pnum]);
-
-//Sort locally.
-qsort_floats(xs);
-
-//SYNCRONIZING
-barrier_wait(arg->bb);
-
-//Copy local array to input.
-long start_idx = sumsizes_start(arg->sizes, arg->pnum);
-long end_idx = sumsizes_end(arg->sizes, arg->pnum); 
-
-//Each process copies its sorted array to input[start...end].
-long idx = start_idx;
-long kk = 0; 
-while (idx < end_idx){
-    //Copying sorted element to file. 
-    arg->data[idx] = xs->data[kk];
-    //Incrementing counters.
-    kk++;			
-    idx++;
-}
-
-free_floats(xs);
-
-free(arg);
-
-return 0;
-}
-
-
-
-
-void
-run_sort_workers(float* data, long size, int P, floats* samps, long* sizes, barrier* bb)
+void qsort_floats(floats *xs)
 {
-pthread_t threads[P];
-
-
-// Spawn P processes, each running sort_worker
-	for(int pp = 0; pp < P; ++pp){
-		job* args = malloc(sizeof(job));
-		args->pnum = pp;
-		args->data = data;
-		args->size = size;
-		args->P = P;
-		args->samps = samps;
-		args->sizes = sizes;
-		args->bb = bb;
-
-		int rv = pthread_create(&(threads[pp]), 0,  sort_worker, args);
-		assert (rv == 0);
-
-
+    qsort(xs->data, xs->size, sizeof(float), comp);
 }
 
-//free(args);
-
-	for(int ii = 0; ii < P; ++ii){
-		int rv = pthread_join(threads[ii], 0);
-		assert (rv == 0);
-    	}
-}
-
-
-
-void
-sample_sort(float* data, long size, int P, long* sizes, barrier* bb)
+floats *
+sample(float *data, long size, int P)
 {
-    floats* samps = sample(data, size, P);
-    run_sort_workers(data, size, P, samps, sizes, bb);
+    floats *xs = make_floats(0);
+
+    floats_push(xs, FLT_MIN);
+
+    int range = 3 * (P - 1);
+    float tmp[range];
+    for (int ii = 0; ii < range; ++ii)
+    {
+        tmp[ii] = data[rand() % size];
+    }
+    qsort(tmp, range, sizeof(float), comp);
+    for (int ii = 1; ii < range; ii += 3)
+    {
+        floats_push(xs, tmp[ii]);
+    }
+
+    floats_push(xs, FLT_MAX);
+
+    return xs;
+}
+
+void *sort_worker(void *arg)
+{
+    sort_job *t_arg = ((sort_job *)arg);
+
+    floats *xs = make_floats(0);
+    for (int ii = 0; ii < t_arg->size; ii++)
+    {
+        if (t_arg->readData[ii] > t_arg->samps->data[t_arg->pnum] && t_arg->readData[ii] <= t_arg->samps->data[t_arg->pnum + 1])
+        {
+            floats_push(xs, t_arg->readData[ii]);
+        }
+    }
+
+    for (int ii = 0; ii < xs->size; ii++)
+    {
+        assert(xs->data[ii] > t_arg->samps->data[t_arg->pnum] && xs->data[ii] <= t_arg->samps->data[t_arg->pnum + 1]);
+    }
+
+    printf("%d: start %.04f, count %ld\n", t_arg->pnum, t_arg->samps->data[t_arg->pnum], xs->size);
+    t_arg->sizes[t_arg->pnum] = xs->size;
+
+    printf("reached 0 %d\n", t_arg->pnum);
+    barrier_wait(t_arg->bb);
+
+    printf("reached 1 %d\n", t_arg->pnum);
+    long start = 0, end = 0;
+    for (int ii = 0; ii < t_arg->pnum; ii++)
+    {
+        start += t_arg->sizes[ii];
+    }
+    for (int ii = 0; ii <= t_arg->pnum; ii++)
+    {
+        end += t_arg->sizes[ii];
+    }
+    // printf("process %d\n", pnum);
+    qsort_floats(xs);
+    // floats_print(xs);
+    barrier_wait(t_arg->bb);
+    printf("reached 2 %d\n", t_arg->pnum);
+    for (int ii = start; ii < end; ii++)
+    {
+        // printf("process %d, stores float %F at %d\n", pnum, xs->data[ii], start + ii);
+        assert(xs->data[ii - start] > t_arg->samps->data[t_arg->pnum] && xs->data[ii - start] <= t_arg->samps->data[t_arg->pnum + 1]);
+        t_arg->writeData[ii] = xs->data[ii - start];
+    }
+    free_floats(xs);
+    free(t_arg);
+    return 0;
+}
+
+void run_sort_workers(float *readData, float *writeData, long size, int P, floats *samps, long *sizes, barrier *bb)
+{
+    pthread_t threads[P];
+
+    for (int pp = 0; pp < P; ++pp)
+    {
+        sort_job *arg = malloc(sizeof(sort_job));
+        arg->pnum = pp;
+        arg->readData = readData;
+        arg->writeData = writeData;
+        arg->size = size;
+        arg->P = P;
+        arg->samps = samps;
+        arg->sizes = sizes;
+        arg->bb = bb;
+
+        int rv = pthread_create(&(threads[pp]), 0, sort_worker, (void *) arg);
+        check_rv(rv);
+    }
+
+    for (int pp = 0; pp < P; ++pp)
+    {
+        int rv = pthread_join(threads[pp], 0);
+        check_rv(rv);
+    }
+}
+
+void sample_sort(float *readData, float *writeData, long size, int P, long *sizes, barrier *bb)
+{
+    floats *samps = sample(readData, size, P);
+    run_sort_workers(readData, writeData, size, P, samps, sizes, bb);
     free_floats(samps);
 }
 
-int
-main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    alarm(95);
+    alarm(120);
 
-    if (argc != 4) {
+    //Usage error is args are not equal to three
+    if (argc != 4)
+    {
         printf("Usage:\n");
-        printf("\t%s P data.dat\n", argv[0]);
+        printf("\t%s P input.dat output.dat\n", argv[0]);
         return 1;
     }
-	
-    //First argument.
-    const int P = atoi(argv[1]); 
-    //Second argument. 
-    const char* fname = argv[2];
-    //Third argument.
-    const char* output = argv[3];
 
+    //Get inputs
+    const int P = atoi(argv[1]);
+    const char *fname = argv[2];
+    const char *fname2 = argv[3];
+
+    //Set seed for the next few random calls
     seed_rng();
 
     int rv;
@@ -498,41 +171,57 @@ main(int argc, char* argv[])
     check_rv(rv);
 
     const int fsize = st.st_size;
-    if (fsize < 8) {
+    if (fsize < 8)
+    {
         printf("File too small.\n");
         return 1;
     }
-	
-    //Open the input file.
+
+    //Open file
     int fd = open(fname, O_RDWR);
+    int fd2 = open(fname2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     check_rv(fd);
+    check_rv(fd2);
 
-
-    //Reading from input file 
+    //Open mmaps
     long count;
     read(fd, &count, 8);
-    float* data = readinput(fd, count);
+    float *fileArray = mmap(0, (count + 2) * sizeof(float), PROT_READ,
+                            MAP_FILE | MAP_SHARED, fd, 0);
 
-    //Sizes for chunk of array in sample sort. 
+    write(fd2, &count, 8);
+    float *writeArray = mmap(0, (count + 2) * sizeof(float), PROT_WRITE,
+                             MAP_FILE | MAP_SHARED, fd2, 0);
+    //Next few bits are for the array data
+    float *readData = &fileArray[2];
+    float *writeData = &writeArray[2];
+
     long sizes_bytes = P * sizeof(long);
-    long* sizes = malloc(sizes_bytes);
-    
-    barrier* bb = make_barrier(P);
+    long *sizes = mmap(0, sizes_bytes, PROT_READ | PROT_WRITE,
+                       MAP_SHARED | MAP_ANONYMOUS, -1, 0); // TODO: This should be shared
 
-    //Sorting the data
-    sample_sort(data, count, P, sizes, bb);
+    // Debugger output
+    // for (size_t i = 0; i < count; i++)
+    // {
+    //     printf("%f\n", data[i]);
+    // }
 
-    //Writting in output file.
-    writeoutput(output, count, data);
+    barrier *bb = make_barrier(P);
+    sample_sort(readData, writeData, count, P, sizes, bb);
 
-    //Freeing. 
     free_barrier(bb);
-    free(data);
-    free(sizes);
 
-    //Closing file descriptor.
+    // for (size_t i = 0; i < count; i++)
+    // {
+    //     printf("%f\n", data[i]);
+    // }
+
+    munmap(sizes, sizes_bytes);
+    munmap(fileArray, (count + 2) * sizeof(float));
+    munmap(writeArray, (count + 2) * sizeof(float));
+
+    //Close File descriptor
+    close(fd2);
     close(fd);
-
     return 0;
 }
-

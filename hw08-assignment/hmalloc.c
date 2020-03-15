57f30cd6fa27d497b2a73975f869d6680ee0fc34
Adwithya <avmagow@gmail.com>
2020-03-15 13:09:51 -0400

    >

--- hw08-assignment/hmalloc.c
+++ hw08-assignment/hmalloc.c
@@ -1,8 +1,239 @@
 
+// #include <stdlib.h>
+// #include <sys/mman.h>
+// #include <stdio.h>
+// #include <assert.h>
+
+// #include "hmalloc.h"
+
+// /*
+//   typedef struct hm_stats {
+//   long pages_mapped;
+//   long pages_unmapped;
+//   long chunks_allocated;
+//   long chunks_freed;
+//   long free_length;
+//   } hm_stats;
+// */
+
+// typedef struct free_block
+// {
+//     size_t size;
+//     struct free_block *next;
+// } free_block;
+
+// const size_t PAGE_SIZE = 4096;
+// static hm_stats stats; // This initializes the stats to 0.
+// static free_block *free_head;
+
+// long free_list_length()
+// {
+//     // TODO: Calculate the length of the free list.
+//     long len = 0;
+//     free_block *curr = free_head;
+//     while (curr)
+//     {
+//         curr = curr->next;
+//         ++len;
+//     }
+//     return len;
+// }
+
+// hm_stats *
+// hgetstats()
+// {
+//     stats.free_length = free_list_length();
+//     return &stats;
+// }
+
+// void hprintstats()
+// {
+//     stats.free_length = free_list_length();
+//     fprintf(stderr, "\n== husky malloc stats ==\n");
+//     fprintf(stderr, "Mapped:   %ld\n", stats.pages_mapped);
+//     fprintf(stderr, "Unmapped: %ld\n", stats.pages_unmapped);
+//     fprintf(stderr, "Allocs:   %ld\n", stats.chunks_allocated);
+//     fprintf(stderr, "Frees:    %ld\n", stats.chunks_freed);
+//     fprintf(stderr, "Freelen:  %ld\n", stats.free_length);
+// }
+
+// static size_t
+// div_up(size_t xx, size_t yy)
+// {
+//     // This is useful to calculate # of pages
+//     // for large allocations.
+//     size_t zz = xx / yy;
+
+//     if (zz * yy == xx)
+//     {
+//         return zz;
+//     }
+//     else
+//     {
+//         return zz + 1;
+//     }
+// }
+
+// //Sort list by address to remove bottleneck of O(n^2)
+// void insert_free_node(free_block *block)
+// {
+//     if (!free_head)
+//     {
+//         free_head->next = block;
+//         return;
+//     }
+//     free_block *cur = free_head;
+//     free_block *prev = 0;
+//     while (cur)
+//     {
+//         if ((void *)cur > (void *)block)
+//         {
+//             if (((void *)prev + prev->size == (void *)block) && ((void *)block + block->size == (void *)cur))
+//             {
+//                 prev->size = prev->size + block->size + cur->size;
+//                 prev->next = cur->next;
+//             }
+//             else if ((void *)prev + prev->size == (void *)block)
+//             {
+//                 prev->size = prev->size + block->size;
+//             }
+//             else if ((void *)block + block->size == (void *)cur)
+//             {
+//                 block->size = block->size + cur->size;
+//                 block->next = cur->next;
+//                 if (prev)
+//                 {
+//                     prev->next = block;
+//                 }
+//                 else
+//                 {
+//                     free_head = block;
+//                 }
+//             }
+//             else
+//             {
+//                 block->next = cur;
+//                 if (prev)
+//                 {
+//                     prev->next = block;
+//                 }
+//                 else
+//                 {
+//                     free_head = block;
+//                 }
+//             }
+//             break;
+//         }
+//         prev = cur;
+//         cur = cur->next;
+//     }
+// }
+
+// /*
+// To allocate memory B bytes of memory, you should first add sizeof(size_t) to B
+// to make space to track the size of the block, and then:
+// For requests with (B < 1 page = 4096 bytes):
+// - See if there's a big enough block on the free list. If so, select the first one and remove it from the list.
+// - If you don't have a block, mmap a new block (1 page = 4096).
+// - If the block is bigger than the request, and the leftover is big enough to store a free list cell, return the extra to the free list.
+// - Use the start of the block to store its size.
+// - Return a pointer to the block *after* the size field.
+// For requests with (B >= 1 page = 4096 bytes):
+// - Calculate the number of pages needed for this block.
+// - Allocate that many pages with mmap.
+// - Fill in the size of the block as (# of pages * 4096).
+// - Return a pointer to the block *after* the size field.
+// */
+
+// void *
+// hmalloc(size_t size)
+// {
+//     stats.chunks_allocated += 1;
+//     size += sizeof(size_t);
+//     size < sizeof(free_block) ? sizeof(free_block) : size;
+
+//     // TODO: Actually allocate memory with mmap and a free list.
+//     if (size < PAGE_SIZE)
+//     {
+//         free_block *block = 0;
+//         free_block *cur = free_head;
+//         free_block *prev = 0;
+//         while (cur)
+//         {
+//             if (cur->size >= size)
+//             {
+//                 block = cur;
+
+//                 if (prev)
+//                 {
+//                     prev->next = cur->next;
+//                 }
+//                 else
+//                 {
+//                     free_head = cur->next;
+//                 }
+//                 break;
+//             }
+//             prev = cur;
+//             cur = cur->next;
+//         }
+
+//         if (!block)
+//         {
+//             block = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+//             assert(block != MAP_FAILED);
+//             stats.pages_mapped += 1;
+//             block->size = PAGE_SIZE;
+//         }
+
+//         if (block->size > size && (block->size - size >= sizeof(free_block)))
+//         {
+//             void *free_address = (void *)block + size;
+
+//             free_block *to_free = (free_block *)free_address;
+//             to_free->size = block->size - size;
+//             block->size = size;
+//             insert_free_node(to_free);
+//         }
+
+//         return (void *)block + sizeof(size_t);
+//     }
+//     else
+//     {
+//         int n_pages = div_up(size, PAGE_SIZE);
+//         free_block *block = mmap(0, n_pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+//         assert(block != MAP_FAILED);
+//         stats.pages_mapped += n_pages;
+//         block->size = n_pages * PAGE_SIZE;
+//         return (void *)block + sizeof(size_t);
+//     }
+// }
+
+// void hfree(void *item)
+// {
+//     stats.chunks_freed += 1;
+
+//     free_block *block = (free_block *)(item - sizeof(free_block));
+
+//     if (block->size < PAGE_SIZE)
+//     {
+//         //TODO put the free block back into the free list.
+//         insert_free_node(block);
+//     }
+//     else
+//     {
+//         int n_pages = div_up(block->size, PAGE_SIZE);
+//         int rv = munmap((void *)block, block->size);
+//         assert(rv != -1);
+//         stats.pages_unmapped += n_pages;
+//     }
+// }
+
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
 
 #include "hmalloc.h"
 
@@ -16,37 +247,43 @@
   } hm_stats;
 */
 
-typedef struct free_block
-{
-    size_t size;
-    struct free_block *next;
-} free_block;
+// Define a struct for a free_list node
+// Contains the size of the
+typedef struct free_list_node {
+  size_t size;
+  struct free_list_node* next;
+} free_list_node;
 
 const size_t PAGE_SIZE = 4096;
 static hm_stats stats; // This initializes the stats to 0.
-static free_block *free_head;
+static free_list_node* free_list_head;
 
-long free_list_length()
+/*
+Caluclate the length of the free list
+*/
+long
+free_list_length()
 {
-    // TODO: Calculate the length of the free list.
-    long len = 0;
-    free_block *curr = free_head;
-    while (curr)
-    {
-        curr = curr->next;
-        ++len;
+    long length = 0;
+    free_list_node* cur = free_list_head;
+
+    while (cur != 0) {
+      length += 1;
+      cur = cur->next;
     }
-    return len;
+
+    return length;
 }
 
-hm_stats *
+hm_stats*
 hgetstats()
 {
     stats.free_length = free_list_length();
     return &stats;
 }
 
-void hprintstats()
+void
+hprintstats()
 {
     stats.free_length = free_list_length();
     fprintf(stderr, "\n== husky malloc stats ==\n");
@@ -57,84 +294,37 @@ void hprintstats()
     fprintf(stderr, "Freelen:  %ld\n", stats.free_length);
 }
 
-static size_t
+static
+size_t
 div_up(size_t xx, size_t yy)
 {
     // This is useful to calculate # of pages
     // for large allocations.
     size_t zz = xx / yy;
 
-    if (zz * yy == xx)
-    {
+    if (zz * yy == xx) {
         return zz;
     }
-    else
-    {
+    else {
         return zz + 1;
     }
 }
 
-//Sort list by address to remove bottleneck of O(n^2)
-void insert_free_node(free_block *node)
-{
-    // if (!free_head)
-    // {
-    //     free_head->next = block;
-    //     return;
-    // }
-    // free_block *cur = free_head;
-    // free_block *prev = 0;
-    // while (cur)
-    // {
-    //     if ((void *)cur > (void *)block)
-    //     {
-    //         if (((void *)prev + prev->size == (void *)block) && ((void *)block + block->size == (void *)cur))
-    //         {
-    //             prev->size = prev->size + block->size + cur->size;
-    //             prev->next = cur->next;
-    //         }
-    //         else if ((void *)prev + prev->size == (void *)block)
-    //         {
-    //             prev->size = prev->size + block->size;
-    //         }
-    //         else if ((void *)block + block->size == (void *)cur)
-    //         {
-    //             block->size = block->size + cur->size;
-    //             block->next = cur->next;
-    //             if (prev)
-    //             {
-    //                 prev->next = block;
-    //             }
-    //             else
-    //             {
-    //                 free_head = block;
-    //             }
-    //         }
-    //         else
-    //         {
-    //             block->next = cur;
-    //             if (prev)
-    //             {
-    //                 prev->next = block;
-    //             }
-    //             else
-    //             {
-    //                 free_head = block;
-    //             }
-    //         }
-    //         break;
-    //     }
-    //     prev = cur;
-    //     cur = cur->next;
-    // }
-     // if the list is empty, just insert it
-  if (free_head == 0) {
-    free_head= node;
+/*
+Inserts the given node into the free_list, ensuring the following variants:
+1. The free list is sorted by memory address of the blocks.
+2. Any two adjacent blocks on the free list get coalesced (joined together) into one bigger block.
+*/
+void
+insert_and_coalesce(free_list_node* node) {
+  // if the list is empty, just insert it
+  if (free_list_head == 0) {
+    free_list_head = node;
     return;
   }
 
-  free_block* cur = free_head;
-  free_block* prev = 0;
+  free_list_node* cur = free_list_head;
+  free_list_node* prev = 0;
   while (cur != 0) {
     // if the current node is greater than the given address,
     // insert the given node after the previous node
@@ -192,7 +382,7 @@ void insert_free_node(free_block *node)
       // If the previous node was null, then we are inserting at the beginning of the free list
       // So we need to update free_list_head
       if (prev == 0) {
-        free_head = node;
+        free_list_head = node;
       }
 
       // break because the node has been inserted or coalesced
@@ -227,87 +417,121 @@ For requests with (B >= 1 page = 4096 bytes):
 - Fill in the size of the block as (# of pages * 4096).
 - Return a pointer to the block *after* the size field.
 */
-
-void *
+void*
 hmalloc(size_t size)
 {
     stats.chunks_allocated += 1;
-    size += sizeof(size_t);
-    size < sizeof(free_block) ? sizeof(free_block) : size;
+    size += sizeof(size_t); //make space to track the size of the block
 
-    // TODO: Actually allocate memory with mmap and a free list.
-    if (size < PAGE_SIZE)
-    {
-        free_block *block = 0;
-        free_block *cur = free_head;
-        free_block *prev = 0;
-        while (cur)
-        {
-            if (cur->size >= size)
-            {
-                block = cur;
+    // For requests less than a page size
+    if (size < PAGE_SIZE) {
+      free_list_node* big_enough_block = 0; //initialize the big enough block to null
+      free_list_node* cur = free_list_head;
+      free_list_node* prev = 0;
+      while (cur != 0) {
+        // TODO remove prints
+        // printf("top of while loop to see if theres a big enough block\n");
+        // printf("size, %li\n", size);
+        // printf("cursize, %li \n", (long) cur->size);
+        // Check if the current block is big enough to store the size
+        // If so, remove it from the list
+        if (cur->size >= size) {
+          big_enough_block = cur; // set big_enough_block to the current block
 
-                if (prev)
-                {
-                    prev->next = cur->next;
-                }
-                else
-                {
-                    free_head = cur->next;
-                }
-                break;
-            }
-            prev = cur;
-            cur = cur->next;
+          // remove the current block from the list
+          if (prev != 0) {
+            // if it's not the first element, set the previous' next to the current next
+            prev->next = cur->next;
+          } else {
+            // if we removed the first element, update the free_list_head
+            free_list_head = cur->next;
+          }
+
+          break;
         }
+        prev = cur; // set the prev node to the current node
+        cur = cur->next; // set the current node to the next node
+      }
 
-        if (!block)
-        {
-            block = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-            assert(block != MAP_FAILED);
-            stats.pages_mapped += 1;
-            block->size = PAGE_SIZE;
-        }
+      // Either there was not a big enough block or the free list was empty
+      // so, mmap a new block of 1 page
+      if (big_enough_block == 0) {
+        //TODO
+        //printf("big_enough_block is null\n" );
+        big_enough_block = mmap(0, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+        assert(big_enough_block != MAP_FAILED);
+        stats.pages_mapped += 1;
+        big_enough_block->size = PAGE_SIZE;
+      }
 
-        if (block->size > size && (block->size - size >= sizeof(free_block)))
-        {
-            void *free_address = (void *)block + size;
+      // Check if the big_enough_block is bigger than the request,
+      // and if the leftover is big enough to store a free list cell
+      if ((big_enough_block->size > size) && (big_enough_block->size - size >= sizeof(free_list_node))) {
+        //TODO
+        //printf("Big enough to store extra!!\n" );
+        // address at which leftover goes into the list
+        void* address = (void*) big_enough_block + size;
 
-            free_block *to_free = (free_block *)free_address;
-            to_free->size = block->size - size;
-            block->size = size;
-            insert_free_node(to_free);
-        }
+        // create a new node from the calculated address
+        free_list_node* leftover = (free_list_node*) address;
+        leftover->size = big_enough_block->size - size;
 
-        return (void *)block + sizeof(size_t);
+        // return the leftover to the free list
+        insert_and_coalesce(leftover);
+
+        // update the big_enough_block size to reflect that the leftover was returned
+        // TODO replace size with big_enough_block->size - leftover->size?
+        big_enough_block->size = size;
+      }
+
+      // Return a pointer to the block *after* the size field.
+      return (void*) big_enough_block + sizeof(size_t);
     }
-    else
-    {
-        int n_pages = div_up(size, PAGE_SIZE);
-        free_block *block = mmap(0, n_pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        assert(block != MAP_FAILED);
-        stats.pages_mapped += n_pages;
-        block->size = n_pages * PAGE_SIZE;
-        return (void *)block + sizeof(size_t);
+
+    // For requests greater than or equal to a page size
+    else {
+      // round up the number of pages through integer division
+      int num_pages = (size + PAGE_SIZE - 1) / PAGE_SIZE;
+
+      // allocate the needed number of pages with mmap
+      size_t size = num_pages * PAGE_SIZE;
+      free_list_node* block = mmap(0, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+      assert(block != MAP_FAILED);
+      stats.pages_mapped += num_pages;
+
+      block->size = size; // fill in the size of the block
+
+      // Return a pointer to the block *after* the size field.
+      return (void*) block + sizeof(size_t);
     }
 }
 
-void hfree(void *item)
+/*
+To free a block of memory,
+first find the beginning of the block by subtracting sizeof(size_t) from the provided pointer.
+If the block is < 1 page then stick it on the free list.
+If the block is >= 1 page, then munmap it.
+*/
+void
+hfree(void* item)
 {
     stats.chunks_freed += 1;
 
-    free_block *block = (free_block *)(item - sizeof(free_block));
+    // find the beginning of the block
+    free_list_node* block = (free_list_node*) (item - sizeof(size_t));
 
-    if (block->size < PAGE_SIZE)
-    {
-        //TODO put the free block back into the free list.
-        insert_free_node(block);
+    // block is less than 1 page in size
+    if (block->size < PAGE_SIZE) {
+      // stick the block back on the free list
+      insert_and_coalesce(block);
     }
-    else
-    {
-        int n_pages = div_up(block->size, PAGE_SIZE);
-        int rv = munmap((void *)block, block->size);
-        assert(rv != -1);
-        stats.pages_unmapped += n_pages;
+
+    // block is greater than or equal to 1 page in size
+    else {
+      // munmap the block
+      int num_pages = (block->size + PAGE_SIZE - 1) / PAGE_SIZE;
+      int rv = munmap((void*) block, block->size);
+      assert(rv != -1);
+      stats.pages_unmapped += num_pages;
     }
 }
